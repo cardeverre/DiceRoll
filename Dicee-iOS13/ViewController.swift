cc12421a25cd968045129f55d723d0d510a50116
JP van Loggerenberg <cardeverre_jp@hotmail.com>
2021-01-08 10:43:37 +0200

    Added DiceRoll view design and button click logic

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -2,19 +2,38 @@
 //  ViewController.swift
 //  Dicee-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by JP Van Loggerenberg. 
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgLeftDice: UIImageView!
+    @IBOutlet weak var imgRightDice: UIImageView!
+    
+    var leftDiceValue = 1
+    var rightDiceValue = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imgLeftDice.image = #imageLiteral(resourceName: "DiceSix")
+        imgLeftDice.alpha = 0.5
+        
+        imgRightDice.image = #imageLiteral(resourceName: "DiceTwo")
+        
     }
 
-
+    @IBAction func btnRoll(_ sender: Any) {
+        //print("Button Pushed")
+        
+        let imgDiceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        
+        imgLeftDice.image = imgDiceArray[Int.random(in:0...5)]
+        imgRightDice.image = imgDiceArray[Int.random(in:0...5)]
+        
+    }
+    
 }
 
